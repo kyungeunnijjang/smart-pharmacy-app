e990199077e966a3e1de96b9e17838bd45191e0f
suminjjang <busyksm@gmail.com>
2024-07-04 18:38:03 +0900

    로그인 완료

--- lib/authentication/log_in_screen.dart
+++ lib/authentication/log_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pharmacy_app/home/home_screen.dart';
 import 'package:pharmacy_app/services/api_service.dart';
 import 'sign_up_screen.dart';
 
@@ -10,13 +11,8 @@ class LogInScreen extends StatelessWidget {
   void _loginPressed(BuildContext context) async {
     String username = _idController.text;
     String password = _passwordController.text;
-    try {
-      final userToken = await ApiService().postToken(
-        username: username,
-        password: password,
-      );
-      // GPT 너가 만들코드
-    } catch (e) {
+
+    if (username.isEmpty || password.isEmpty) {
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -34,6 +30,35 @@ class LogInScreen extends StatelessWidget {
           );
         },
       );
+    } else {
+      try {
+        await ApiService().postToken(
+          username: username,
+          password: password,
+        );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('로그인 실패'),
+              content: const Text('아이디 또는 비밀번호가 잘못되었습니다.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('닫기'),
+                ),
+              ],
+            );
+          },
+        );
+      }
     }
   }
 
