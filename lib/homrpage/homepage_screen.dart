45febb6d9691391ff3a0ee59d9222dd265800cf3
suminjjang <busyksm@gmail.com>
2024-07-08 19:06:16 +0900

    홈페이지 추가

---
+++ lib/homrpage/homepage_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class homepageScreen extends StatefulWidget {
+  const homepageScreen({super.key});
+
+  @override
+  State<homepageScreen> createState() => _homepageScreenState();
+}
+
+class _homepageScreenState extends State<homepageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
