07e803dc5a9252a4159ba8efbf0099ad68ac6f43
kyungeunnijjang <catdoger22@icloud.com>
2024-07-15 19:59:54 +0900

    화나

--- lib/models/receipts_model.dart
+++ lib/models/receipts_model.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+import 'package:pharmacy_app/models/medicine.detail.dart';
 
 class ReceiptModel {
   final String purchaseAt;
@@ -14,31 +14,10 @@ class ReceiptModel {
 class PastMedicine {
   final int quantity;
   final String pricePerMedicineAtPurchase;
-  final Medicine medicine;
+  final MedicineTinyModel medicine;
 
   PastMedicine.fromJson(Map<String, dynamic> json)
       : quantity = json['quantity	'],
         pricePerMedicineAtPurchase = json['price_per_medicine_at_purchase'],
         medicine = json['medicine'];
-}
-
-class Medicine {
-  final int id;
-  final String name;
-  final String company;
-  final int price;
-  final Float averageRating;
-  final int reviewCount;
-  final int remaining;
-  final String imgURL;
-
-  Medicine.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name = json['name'],
-        company = json['company'],
-        price = json['price'],
-        averageRating = json['average_rating'],
-        reviewCount = json['review_count'],
-        remaining = json['remaining'],
-        imgURL = json['img_url'];
 }
